1511d1084781b58132820295e1cd3702fe90e9ff
Gabriel Davidenko <gabriel.davidenko@gmail.com>
2023-03-26 14:48:49 +0200

    feat: add helper

---
+++ src/components/helper.vue
@@ -0,0 +1,33 @@
+<template>
+    <h2 class="text-center">Page d'aide</h2>
+
+
+    <h3>Principe</h3>
+    <p>
+        All will be one est une application de compte partagée.
+    </p>
+    <h3>Création de compte</h3>
+    <p>
+        Pour créer un compte, vous devrez renseigner un libelle et une description.
+        Une fois le compte créé, cliquez sur le libelle du compte vous ménera sur la page de balance du compte
+    </p>
+    <h3>Balance du groupe</h3>
+    <p>
+        Sur la page de balance du compte, vous porez voir le montant du compte, et le montant du de chaque personne.
+    </p>
+    <h3>Gestion des membres</h3>
+    <p>
+        La page de gestion des membres vous permet de voir les membres du compte, en ajouter, ou retirer si ils n'ont
+        pas de dépenses à leur actif.
+
+    </p>
+    <h3>Dépenses</h3>
+    <p>
+        La page des dépenses vous permet d'ajouter une dépense à un membre du groupe, et de voir les dépenses du compte
+    </p>
+</template>
+<script>
+export default {
+    name: 'helpComponent'
+}
+</script>
