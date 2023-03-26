02fda0d3d8b8aebc9320c00e6eb339eca7d7234d
Gabriel Davidenko <gabriel.davidenko@gmail.com>
2023-03-26 14:48:17 +0200

    feat: add account vue

--- src/components/compte-partage.vue
+++ src/components/compte-partage.vue
@@ -1,26 +1,39 @@
 <template>
-    <h2>Ajouter un compte</h2>
-    <div>
-        <label for="flibelle">libelle</label>
-        <input id="flibelle" placeholder="Libelle de votre compte" v-model="libelle" />
-        <br>
-        <label for="fdescription">description</label>
-        <textarea id="fdescription" placeholder="Description de votre compte" v-model="description" />
-        <br>
-        <button @click="createAccount()">Ajouter le compte</button>
+    <div class="row">
+        <h2 class="col">Ajouter un compte</h2>
+        <div class="row">
+            <label class="form-label" for="flibelle">libelle</label>
+            <input class="form-control" id="flibelle" placeholder="Libelle de votre compte" v-model="libelle" />
+            <br>
+            <label class="form-label" for="fdescription">description</label>
+            <textarea class="form-control" id="fdescription" placeholder="Description de votre compte" v-model="description"></textarea>
+            <br>
+            <button type="button" class="btn btn-primary" :disabled="formValidator" @click="createAccount()">Ajouter le
+                compte</button>
+        </div>
     </div>
-    <ul>
-        <li v-for="(account, index) in accountList" :key="index">
-        libelle : <router-link v-bind:to="'/gestion-membre/'+ account.id">{{ account.libelle }}</router-link> - 
-        description : {{ account.description }} - 
-        date de creation : {{account.createdAt}}
-    </li>
-    </ul>
+    <div class="row">
+        <table class="table table-stripedtable table-striped">
+            <thead>
+                <tr>
+                    <th>libelle</th>
+                    <th>description</th>
+                    <th>Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="account in accountList" :key="account.id">
+                    <td><router-link v-bind:to="'/compte/' + account.id">{{ account.libelle }}</router-link></td>
+                    <td>{{ account.description }}</td>
+                    <td>{{ account.createdAt }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
 import { v4 as uuidv4 } from 'uuid';
-
 /**
  * @todo mettre une sécurité pour ne pas ajouter si il manque un champ 
  * listener enter pour ajouter
@@ -28,14 +41,12 @@ import { v4 as uuidv4 } from 'uuid';
  */
 export default {
     name: 'comptePartage',
+
     data() {
         return {
             accountList: [],
             libelle: '',
             description: '',
-            optionsDate: {
-                weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'
-            }
         }
     },
     methods: {
@@ -48,7 +59,8 @@ export default {
                 id: uuidv4(),
                 libelle: this.libelle,
                 description: this.description,
-                createdAt: new Date().toLocaleDateString('fr-FR',this.optionsDate),
+                montant: 0,
+                createdAt: new Date().toLocaleString('fr-FR', { timeZone: 'UTC' }),
             });
             this.resetForm();
             this.saveInLocalStorage();
@@ -68,7 +80,22 @@ export default {
             localStorage.setItem("accountList", JSON.stringify(this.accountList));
         }
     },
+    computed: {
+        /**
+         * calcule si le formulaire est valide ou non
+         */
+        formValidator() {
+            if (!this.libelle) {
+                return true
+            }
+            if (!this.description) {
+                return true
+            }
+            return false
+        }
+    },
     mounted() {
+        //récupère la liste des comptes dans le local storage
         if (localStorage.getItem('accountList')) {
             try {
                 this.accountList = JSON.parse(localStorage.getItem('accountList'));
